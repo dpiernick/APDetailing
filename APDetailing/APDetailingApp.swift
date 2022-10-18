4ee64d08ff217a35eab7c4f2a7f5a13769961478
Dave Piernick <dpiernick@gmail.com>
2022-10-18 12:22:42 -0600

    Initial Commit

---
+++ APDetailing/APDetailingApp.swift
@@ -0,0 +1,17 @@
+//
+//  APDetailingApp.swift
+//  APDetailing
+//
+//  Created by Dave Piernick on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct APDetailingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
